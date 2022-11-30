dea0a74fc41b3c9b42c5c94d8fa6f04467bf10b3
Jasser12544 <jasser.bawi19@gmail.com>
2022-11-30 15:44:54 +0000

    Initial commit, adding README and script.

---
+++ README.md
@@ -0,0 +1,60 @@
+# Title
+### Subtitle
+###### Smallest subtitle
+
+## Text
+
+Hello 2.0, *Emphasised*, **Strong**
+
+_italics_, __Bold__, ___Bold and Italics___ or __*Bold and Italics*__
+
+_Italics and **Bold**_
+
+## Quotes
+
+>Something Profound
+>>The Great Philosopher Me
+>>>More Quote
+
+## Lists
+- This
+- Is a
+- Simple
+- List
+
+* More
+* List
+
+1. One
+2. Two
+3. Three
+123123123. Four
+
+## Images
+![Man](C:/Users/Jasser/Pictures/Jasser/Back.jpg)
+
+<img src="C:/Users/Jasser/Pictures/Jasser/Back.jpg" width="100" height="100">
+
+## Links
+
+[Markdown Cheatsheet](https://enterprise.github.com/downloads/en/markdown-cheatsheet.pdf)
+
+- [Title](#Title)
+- [Quotes](##Quotes)
+- [Images](##Images)
+
+## Github markdown
+
+`This a code snippet`
+```C#
+int i = 10 +c;
+```
+- [ ] Not Done.
+- [X] Done.
+
+***Name*** | ***Street*** | ***Job***
+|
+Tim | Legit  Lane | Coder
+John | Legit Road | Programmer
+Todd | Real house | Tamer
+Ben | Real lane | Driver
---
+++ example-script.ps1
@@ -0,0 +1,44 @@
+﻿echo "This is a hello world batch file `n"
+# Create a folder if it does not already exist
+if (Get-Item -Path FolderA -ErrorAction Ignore) {
+    echo "Main folder already exists `n"
+    cd FolderA
+}
+else {
+    echo "Creating the main folder `n"
+    md FolderA
+    cd FolderA
+}
+
+# Create a file
+echo "Hello World" > testfile.txt
+
+# Append Content to the file
+echo "New Line" >> testfile.txt
+echo A B C D >> testfile.txt
+
+# Add content to the file - this overwrites the previous contents
+echo "Last line" >> testfile.txt
+
+# Display file contents
+echo "`nContent of the Text File after appending text: `n"
+cat testfile.txt
+
+if (Get-Item -Path SubFolder -ErrorAction Ignore) {
+    echo "`nSub Folder already exists in main folder `n"
+}
+else {
+    echo "`nCreating sub folder `n"
+    md SubFolder
+}
+
+# move to sub folder directory
+cd .\SubFolder
+echo "This is a text file in the sub folder" > introduction.txt
+
+# Displays sub folder text file content
+echo "`nContent of the Sub folder text file: `n"
+cat introduction.txt
+
+# move back to parent directory
+cd ../..
