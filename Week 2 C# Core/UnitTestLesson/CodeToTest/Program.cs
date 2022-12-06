106f581dfa80c4cdb8c2403d319dce6705e5dcc5
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-06 15:22:38 +0000

    Initial CodeToTest Commit

---
+++ Week 2 C# Core/UnitTestLesson/CodeToTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeToTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
