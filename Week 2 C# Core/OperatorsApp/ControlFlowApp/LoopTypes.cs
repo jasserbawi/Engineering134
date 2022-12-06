86480dba2141210fc002993f613f095e34f9d16a
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-06 15:21:29 +0000

    Commit

---
+++ Week 2 C# Core/OperatorsApp/ControlFlowApp/LoopTypes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowApp
+{
+    public static class LoopTypes
+    {
+        internal static int HighestDoWhileLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            int i = 0;
+            do
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+                i++;
+            }
+            while (i < nums.Count());
+            return highest;
+        }
+
+        internal static int HighestForEachLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            foreach (int i in nums)
+            {
+                if (i > highest)
+                {
+                    highest = i;
+                }
+            }
+            return highest;
+        }
+
+        internal static int HighestForLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+            }
+            return highest;
+        }
+
+        internal static int HighestWhileLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            int i = 0;
+            while (i < nums.Count())
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+                i++;
+            }
+            return highest;
+        }
+    }
+}
--- Week 2 C# Core/OperatorsApp/ControlFlowApp/Program.cs
+++ Week 2 C# Core/OperatorsApp/ControlFlowApp/Program.cs
@@ -4,12 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int mark = 88;
-            var grade = mark >= 65 ? (mark >= 85 ? "distinction" : "Pass") : "Fail";
-            Priority(3);
+            /*            int mark = 88;
+                        var grade = mark >= 65 ? (mark >= 85 ? "distinction" : "Pass") : "Fail";
+                        Priority(3);
+
+                        Console.WriteLine(CanDrive(12));*/
+
+            List<int> nums = new List<int> { -10, -6, -22, -17, -3 };
+            Console.WriteLine("Highest foreach loop: " + LoopTypes.HighestForEachLoop(nums));
+            Console.WriteLine("Highest for- loop: " + LoopTypes.HighestForLoop(nums));
+            Console.WriteLine("Highest while- loop: " + LoopTypes.HighestWhileLoop(nums));
+            Console.WriteLine("Highest do-while loop: " + LoopTypes.HighestDoWhileLoop(nums));
         }
 
-        public static string Priority(int level)
+/*        public static string Priority(int level)
         {
             string priority = "Code ";
             switch (level)
@@ -35,12 +43,12 @@
         {
             switch(age)
             {
-                case (age >= 17):
+                case >= 17:
                     return true;
                 default:
                     return false;
 
             }
-        }
+        }*/
     }
 }
