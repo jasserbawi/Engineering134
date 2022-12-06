b1153bebae8808d30c84a37debbf2f9496be91de
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-06 10:20:25 +0000

    Initial commit of OperatorsApp

---
+++ Week 2 C# Core/OperatorsApp/OperatorsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OperatorsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
