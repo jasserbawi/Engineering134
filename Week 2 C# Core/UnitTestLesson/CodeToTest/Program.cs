a52a6d8ae4e100e5d65150825aae499a5e464f6c
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-06 15:50:24 +0000

    Refactored the code

--- Week 2 C# Core/UnitTestLesson/CodeToTest/Program.cs
+++ Week 2 C# Core/UnitTestLesson/CodeToTest/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace CodeToTest
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int timeOfDay = 21;
+            Console.WriteLine(GetGreeting(timeOfDay));
+            
+        }
+
+        private static string GetGreeting(int timeOfDay)
+        {
+            string message;
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                message = "Good morning!";
+            }
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                message = "Good afternoon!";
+            }
+            else
+            {
+                message = "Good evening!";
+            }
+            
+            return message;
         }
     }
 }
