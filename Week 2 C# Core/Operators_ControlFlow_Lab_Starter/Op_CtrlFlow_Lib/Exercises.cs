a3e416f861084272ef3f8d6d3e81919f9a3be7d3
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-07 17:58:57 +0000

    Operators and Control flow lab initial commit

---
+++ Week 2 C# Core/Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Op_CtrlFlow
+{
+    public class Exercises
+    {
+        public static bool MyMethod(int num1, int num2)
+        {
+            return num1 == num2 ? false : (num1 % num2) == 0;
+        }
+
+        // returns the average of the array nums
+        public static double Average(List<int> nums)
+        {
+            int average;
+            var nums = new List<int>() { 3, 8, 1, 7, 3 };
+            for (i = 0; i < nums.Count; i++)
+            {
+                average + i
+            }
+            return average/nums.Count;
+        }
+
+        // returns the type of ticket a customer is eligible for based on their age
+        // "Standard" if they are between 18 and 59 inclusive
+        // "OAP" if they are 60 or over
+        // "Student" if they are 13-17 inclusive
+        // "Child" if they are 5-12
+        // "Free" if they are under 5
+        public static string TicketType(int age)
+        {
+            string ticketType = string.Empty;
+            return ticketType;
+        }
+
+        public static string Grade(int mark)
+        {
+            var grade = "";
+            return grade;
+        }
+
+        public static int GetScottishMaxWeddingNumbers(int covidLevel)
+        {
+            return 0;
+        }
+    }
+}
