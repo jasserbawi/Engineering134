cca49937b04db3bbd2ec2ae66c8625975bf75d73
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-08 16:03:06 +0000

    String Builder in progress

---
+++ Week 2 C# Core/MoreDataTypesApp/MoreDataTypesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MoreDataTypesApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var myString = " C# list fundamentals";
+        Console.WriteLine(StringExercise(myString));
+    }
+
+    public static string StringExercise(string myString)
+    {
+        var intermediate = myString.Trim().ToUpper().Replace('L', '*').Replace('T', '*');
+        var nPos = intermediate.IndexOf('N');
+        var finalString = intermediate.Remove(nPos);
+        return finalString;
+    }
+}
