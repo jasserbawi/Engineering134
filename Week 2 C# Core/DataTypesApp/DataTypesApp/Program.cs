e0cb470c75de2f04c5241644697246c81960e403
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-08 11:05:09 +0000

    Created DataTypesApp.

---
+++ Week 2 C# Core/DataTypesApp/DataTypesApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTypesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
