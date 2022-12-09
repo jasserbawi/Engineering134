cf8bd9a28a78fd61d4850422bf81249c3b552448
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-09 14:23:02 +0000

    Commit fixes

--- Week 2 C# Core/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ Week 2 C# Core/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -8,19 +8,62 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            string[] output = new string[contents.Count];
+            for (int i = 0; i < contents.Count; i++)
+            {
+                output[i] = contents[i];
+            }
+            return output;
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            if (length1 +length2 +length3 != contents.Count)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+
+            string[,,] output = new string[length1, length2, length3];
+            int index = 0;
+            for (int x = 0; x < length1; x++)
+            {
+                for (int y = 0; y < length2; y++)
+                {
+                    for (int z = 0; z < length3; z++)
+                    {
+                        output[x, y, z] = contents[index++];
+                    }
+                }
+            }
+            return output;
         }
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
+            if(countRow1 + countRow2 != contents.Count)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+            string[][] output = new string[2][];
+            output[0] = new string[countRow1];
+            output[1] = new string[countRow2];
+            int index = 0;
+            foreach(string element in contents)
+            {
+                if (index < countRow1)
+                {
+                    output[0][index] = element;
+                }
+                else
+                {
+                    
+                    output[1][index - countRow1] = element;
+                }
+                index++;
+                }
+        return output;
         }
     }
 }
---
+++ Week 2 C# Core/MoreTypes_Lab_Starter/MoreTypes_Lab/Reference Lesson/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+namespace MoreDataTypesApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ONeDArrays();
+
+        MultiDArrays();
+
+        JaggedArrays();
+    }
+
+
+    public static void ONeDArrays()
+    {
+        int[] myIntArray = { 12, 23, 34, 45, 56 };
+
+        Array.Reverse(myIntArray); // using Array.Reverse(the array) we can reverse the array
+
+        foreach (var element in myIntArray.Reverse()) // or you can reverse the array here using the method .Reverse() for arrays
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+    public static void MultiDArrays()
+    {
+        //2D Arrays
+        int[,] grid = new int[2, 4]; // rows and columns which are indexed, so when referred to you need to understand that 4 rows means 0, 1, 2, 3
+        grid[0, 1] = 6;
+        grid[1, 0] = 8;
+        grid[1, 3] = 10;
+
+        char[,] gridTwo =
+        {
+            {'a', 'b' },
+            {'c', 'd' },
+            {'e', 'f' },
+            {'e', 'f' },
+            {'g', 'h' },
+            /*{'i', 'j', 'k' },*/ //this would not work because arrays are set and cannot be changed
+        };
+
+        Console.WriteLine("All of grid two");
+
+        foreach (var element in gridTwo)
+        {
+            Console.WriteLine(element);
+        }
+
+        for (int i = gridTwo.GetLowerBound(0); i <= gridTwo.GetUpperBound(0); i++)
+        {
+            for (int j = gridTwo.GetLowerBound(1); j <= gridTwo.GetUpperBound(1); i++)
+            {
+                /*Console.WriteLine($"({i}, {j}) {gridTwo[i, j]} ");*/
+            }
+        }
+
+    }
+    public static void JaggedArrays()
+    {
+        int[][] intJArray = new int[2][]; // we can initialise jaggedarrays as we are declaring
+        intJArray[0] = new int[4]; // we declare the number of variables in the position 0 out of the 2 declared at the start
+        intJArray[1] = new int[2];
+
+        intJArray[0][2] = 3;
+        intJArray[1][0] = 5;
+
+        string[][] animalJArray = new string[][]
+        {
+            new string[] {"Hyena", "Lion", "Cheetah", "Panther"},
+            new string[] { "Peacock", "Canary" },
+        };
+
+        Console.WriteLine("Jagged array of animals");
+        foreach (var element in animalJArray)
+        {
+            foreach (var animal in element)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
