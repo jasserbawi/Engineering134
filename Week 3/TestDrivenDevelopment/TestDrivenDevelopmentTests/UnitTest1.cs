163ae3b132fd126f7d39509a29f39cf83c4af6d5
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-16 11:03:45 +0000

    TDD app created

---
+++ Week 3/TestDrivenDevelopment/TestDrivenDevelopmentTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestDrivenDevelopmentTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
