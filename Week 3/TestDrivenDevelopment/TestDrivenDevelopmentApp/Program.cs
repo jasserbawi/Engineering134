819f148e61d761eaf03b088b3e7ddce4a1758230
Jasser12544 <jasser.bawi19@gmail.com>
2022-12-16 12:20:11 +0000

    Commit

--- Week 3/TestDrivenDevelopment/TestDrivenDevelopmentApp/Program.cs
+++ Week 3/TestDrivenDevelopment/TestDrivenDevelopmentApp/Program.cs
@@ -1,10 +1,72 @@
-﻿namespace TestDrivenDevelopmentApp
-{
-    internal class Program
+﻿using Microsoft.VisualBasic;
+
+namespace TestDrivenDevelopmentApp;
+
+public class Program
+{   
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
     }
+
+
+
+    // argument is the value passed in when a method is called
+
+    public static string FizzBuzz(int maxValue)
+    {
+        string outputString = "";
+        for (int i = 1; i <= maxValue; i++)
+        {
+            if (i % 3 == 0)
+            {
+                outputString += "Fizz ";
+            }
+            else if (i % 5 == 0)
+            {
+                outputString += "Buzz ";
+            }
+            else
+            {
+                outputString += i + " ";
+            }
+        }
+        return outputString.TrimEnd();
+    }
+
+
+    /*            The problem -FizzBuzz
+    We want to write an application which:
+
+    Outputs the integers from one to some maximum value as a sequence of strings "1", "2", ….
+    If the integer is divisible by 3, it is replaced by "Fizz"
+    If the integer is divisible by 5, it is replaced by "Buzz"
+    If it is divisible by both 3 and 5, it is replaced by "FizzBuzz"
+
+    Expected output for a maximum value of 30:
+
+    1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17
+    Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz*/
+
+    /*
+        Requirements:
+    Takes in max value
+
+    Given 1, returns the first number, 1
+
+    Given 2, returns number, then 2
+
+    Given some maximum value return all numbers in a row
+
+    Given 3 FizzBuzz returns "1 2 Fizz"
+
+    Prints outputs
+     
+     */
+
+
+
+
+
+
 }
--- Week 3/TestDrivenDevelopment/TestDrivenDevelopmentTests/UnitTest1.cs
+++ Week 3/TestDrivenDevelopment/TestDrivenDevelopmentTests/UnitTest1.cs
@@ -1,16 +1,44 @@
-namespace TestDrivenDevelopmentTests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using TestDrivenDevelopmentApp;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+namespace TestDrivenDevelopmentTests;
+
+public class TDDTests
+{
+    [Test]
+    public void Given1_FizzBuzz_Returns1()
+    {
+        int maxvalue = 1;
+        Assert.That(Program.FizzBuzz(maxvalue), Is.EqualTo("1"));
+    }
+
+    [Test]
+    public void Given2_FizzBuzz_Returns1AndThen2()
+    {
+        int maxvalue = 2;
+        Assert.That(Program.FizzBuzz(maxvalue), Is.EqualTo("1 2"));
+    }
+
+    [Test]
+    public void GivenSomeMaximumValue_FizzBuzz_ReturnsAllNumbersInARow()
+    {
+        int maxvalue = 10;
+        string expectedValue = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz";
+        Assert.That(Program.FizzBuzz(maxvalue), Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void Given3_FizzBuzz_Returns12Fizz()
+    {
+        int maxvalue = 3;
+        string expectedValue = "1 2 Fizz";
+        Assert.That(Program.FizzBuzz(maxvalue), Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void Given4_FizzBuzz_Returns1BuzzFizzBuzz()
+    {
+        int maxvalue = 5;
+        string expectedValue = "1 2 Fizz 4 Buzz";
+        Assert.That(Program.FizzBuzz(maxvalue), Is.EqualTo(expectedValue));
     }
 }
